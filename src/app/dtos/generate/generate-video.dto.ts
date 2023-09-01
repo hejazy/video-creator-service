49889dca44b65818b648f50c17c8a170bd15b7c8
hejazy <hegazy@outlook.com>
2023-09-02 00:49:10 +0300

    UPD: add the validator

---
+++ src/app/dtos/generate/generate-video.dto.ts
@@ -0,0 +1,13 @@
+import Joi from 'joi';
+
+
+export const GenreateVideoSchemaFactory = ({}) => {
+  const schema: Joi.SchemaMap = {
+    images: Joi.array().items(Joi.object({
+      url: Joi.string(),
+      delay: Joi.number()
+    })),
+  };
+  return Joi.object().keys(schema)
+};
+export const GenreateVideoSchema = GenreateVideoSchemaFactory({});
